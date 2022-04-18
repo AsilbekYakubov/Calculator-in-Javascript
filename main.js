9fa36d215dbf4b657a7568d15f642c8e0e049612
Web developer <asilbekyakubov0525@gmail.com>
2022-04-18 16:25:04 +0500

    error comit

---
+++ main.js
@@ -0,0 +1,50 @@
+let aa = document.getElementById('input');
+
+function nom() {
+    aa.value += "7"
+}
+function nom2() {
+    aa.value += "8"
+}
+function nom3() {
+    aa.value += "9"
+}
+function nom4() {
+    aa.value = ""
+}
+function nom5() {
+    aa.value += "4"
+}
+function nom6() {
+    aa.value += "5"
+}
+function nom7() {
+    aa.value += "6"
+}
+function nom8() {
+    aa.value += "*"
+}
+function nom9() {
+    aa.value += "1"
+}
+function nom10() {
+    aa.value += "2"
+}
+function nom11() {
+    aa.value += "3"
+}
+function nom12() {
+    aa.value += "-"
+}
+function nom13() {
+    aa.value += "0"
+}
+function nom14() {
+    aa.value += "/"
+}
+function nom15() {
+    aa.value += "+"
+}
+function nom16() {
+    aa.value = eval(aa.value)
+}
